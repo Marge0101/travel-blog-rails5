2a3c1bd1eea241e4e8a7229bfcc87717ce3ca307
Mari Sato <marge0101.ms@gmail.com>
2018-02-09 05:21:15 +0000

    send grid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'travel-app-test.herokuapp.com'
+  host = 'test-trav.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   
   
